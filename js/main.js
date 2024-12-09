e0b3d805d5627333c55566e0ef3a3b4638ced4e0
ccarlos.llima@gmail.com <ccarlos.llima@gmail.com>
2024-12-09 16:21:30 -0300

    atualizacao

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-// scripts.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Adicione aqui qualquer JavaScript necessário para funcionalidade adicional.
-});
- 
+//==================================================================================
+// FORMULARIO DINAMICO
+//==================================================================================
+
+// Pegando os elementos necessários
+var modal = document.getElementById("myModal");
+// var openModalBtn = document.getElementById("openModalBtn");
+var closeModal = document.getElementsByClassName("close")[0];
+
+function openModal() {
+    modal.style.display = "block";
+}
+// Quando o usuário clica no botão de fechar (x), o modal é escondido
+closeModal.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Quando o usuário clica fora do modal, ele também fecha
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//==================================================================================
+// FIM DO FORMULÁRIO
+//==================================================================================
+
+
+//==================================================================================
+// SLIDER DE CURSO
+//==================================================================================
